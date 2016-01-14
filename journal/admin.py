08c1ab913518a040eeef34aa47cf2e866c392e92
Stuart Quin <stuart.quin@gmail.com>
2016-01-14 21:42:57 +0000

    Tidy up admin interface

--- journal/admin.py
+++ journal/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import Thought, Tag
 
-admin.site.register(Thought)
-admin.site.register(Tag)
+
+@admin.register(Thought)
+class ThoughtAdmin(admin.ModelAdmin):
+    list_display = ('content', 'user', 'created_at')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('tag', 'user', 'created_at')
