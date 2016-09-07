cc44d2f630ba5ef57ded714023783641721476a6
Stuart Quin <stuart.quin@gmail.com>
2016-09-07 08:43:23 +0100

    Styles

--- journal/static/journal/src/nav/navmenu.js
+++ journal/static/journal/src/nav/navmenu.js
@@ -5,21 +5,17 @@ import styles from './styles.css';
 
 const NavMenu = (props) => {
   'use strict';
-  const navClasses = `nav navbar-nav navbar-right ${styles.navbarNav}`;
+  const navClasses = `nav navbar-nav ${styles.navbarNav}`;
 
   return (
     <ul className={ navClasses }>
-      <li>
-        <Link to={'/timeslips'}
-            className={styles.navItem}
-            activeClassName={styles.navActive}>
+      <li className={styles.navItem}>
+        <Link to={'/timeslips'} activeClassName={styles.navActive}>
           Tracking
         </Link>
       </li>
-      <li>
-        <Link to={'/invoices'}
-            className={styles.navItem}
-            activeClassName={styles.navActive}>
+      <li className={styles.navItem}>
+        <Link to={'/invoices'} activeClassName={styles.navActive}>
           Invoices
         </Link>
       </li>
