c9352b2cbb8b42fd97358f2c710c8a8dabafa46c
Stuart Quin <stuart.quin@gmail.com>
2016-09-17 09:27:08 +0100

    Production webpack config

--- journal/static/journal/webpack.config.js
+++ journal/static/journal/webpack.config.js
@@ -1,28 +1,35 @@
 var path = require('path');
 var webpack = require('webpack');
 
-var definePlugin = new webpack.DefinePlugin({
-  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
-  __USERNAME__: JSON.stringify(process.env.USERNAME || null),
-  __PASSWORD__: JSON.stringify(process.env.PASSWORD || null)
-});
+var IS_DEV = JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true'));
+
+var plugins = [
+  new webpack.DefinePlugin({
+    __DEV__: IS_DEV,
+    __USERNAME__: JSON.stringify(process.env.USERNAME || null),
+    __PASSWORD__: JSON.stringify(process.env.PASSWORD || null)
+  })
+];
+
+var entry = [
+  './src/index'
+];
+if (IS_DEV) {
+  plugins.push(new webpack.HotModuleReplacementPlugin());
+
+  entry.push('webpack-dev-server/client?http://localhost:3000');
+  entry.push('webpack/hot/only-dev-server');
+}
 
 module.exports = {
   devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './src/index'
-  ],
+  entry: entry,
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    definePlugin
-  ],
+  plugins: plugins,
   module: {
     loaders: [{
       test: /\.js$/,
