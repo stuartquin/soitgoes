c64e894dcf1bbafbe8c459ebc0f9712b5fe1ddef
Stuart Quin <stuart.quin@gmail.com>
2016-09-21 13:56:23 +0100

    Color updates

--- journal/static/journal/src/components/invoices/invoicelist.js
+++ journal/static/journal/src/components/invoices/invoicelist.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router';
-import moment from 'moment';
 
-import styles from './styles.css';
+import { InvoiceListRow } from './invoicelistrow';
 
 const InvoiceList = (props) => {
   return (
@@ -11,22 +9,12 @@ const InvoiceList = (props) => {
         <tr>
           <th>Invoice</th>
           <th>Issued</th>
-          <th>Due</th>
-          <th>Paid</th>
+          <th>Total</th>
         </tr>
       </thead>
       <tbody>
       {props.invoices.map(invoice => (
-        <tr>
-          <td>
-            <Link to={`/invoices/${invoice.id}`}>
-            {invoice.project.name} #{invoice.sequence_num}
-            </Link>
-          </td>
-          <td>{invoice.issued_at ? moment(invoice.issued_at).format('YYYY-MM-DD') : '-'}</td>
-          <td>&pound;{invoice.total_due}</td>
-          <td>&pound;{invoice.total_paid}</td>
-        </tr>
+        <InvoiceListRow invoice={invoice} />
       ))}
       </tbody>
     </table>
---
+++ journal/static/journal/src/components/invoices/invoicelistrow.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router';
+import moment from 'moment';
+
+import styles from './styles.css';
+
+const InvoiceListRow = (props) => {
+  const invoice = props.invoice;
+  const issuedAt = invoice.issued_at ? moment(invoice.issued_at).format('YYYY-MM-DD') : '-';
+
+  let totalClass = 'text-danger';
+  let total = invoice.total_due;
+  if (invoice.total_paid) {
+    totalClass = 'text-success';
+    total = invoice.total_paid;
+  }
+
+  return (
+    <tr className={ styles.invoiceListRow }>
+      <td>
+        <Link to={`/invoices/${invoice.id}`}>
+        {invoice.project.name} #{invoice.sequence_num}
+        </Link>
+      </td>
+      <td>
+        {issuedAt}
+      </td>
+      <td>
+        <span className={ totalClass }>&pound;{total}</span>
+      </td>
+    </tr>
+  );
+};
+
+export {InvoiceListRow};
