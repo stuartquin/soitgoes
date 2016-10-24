a731b835c77e916411deae4e034566c68b192918
Stuart Quin <stuart.quin@gmail.com>
2016-10-24 18:50:41 +0100

    Robust date format checking

--- libs/monzo.py
+++ libs/monzo.py
@@ -1,10 +1,13 @@
 import os
+import logging
 from django.core.exceptions import ObjectDoesNotExist
 
 from journal.models import Expense
 from datetime import datetime
 
 MONZO_ACC = os.getenv('MONZO_ACC')
+
+logger = logging.getLogger(__name__)
 
 
 def get_existing_expense(monzo_id):
@@ -26,10 +29,13 @@ def import_transaction(transaction):
     # @TODO need to work out the user-id here
     expense.user_id = 1
     expense.type = transaction['category']
-    expense.date = datetime.strptime(
-        transaction['created'],
-        '%Y-%m-%dT%H:%M:%SZ'
-    )
+
+    created = transaction['created']
+    try:
+        expense.date = datetime.strptime(created, '%Y-%m-%dT%H:%M:%S.%fZ')
+    except ValueError:
+        logger.error('Unrecognised date format: %s' % created)
+
     expense.value = transaction['amount'] / 100.0
     expense.monzo_id = transaction['id']
     expense.reference = transaction['description']
