68fca0c17abdb412014cff967b14acf59e87ed11
Stuart Quin <stuart.quin@gmail.com>
2019-10-16 13:59:05 +0100

    Fix task table header

--- client/src/components/Tasks/TaskTable/TaskTableHeader.js
+++ client/src/components/Tasks/TaskTable/TaskTableHeader.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Box } from 'rebass/styled-components';
 
-import {Grid, Cell, CellMd} from 'components/Grid';
 import {Header} from 'components/DataTable';
 
 
 const TaskTableHeader = () => (
   <Header>
-    <Cell xs="7" sm="6">
+    <Box>
       Task
-    </Cell>
-    <CellMd numeric sm="2">
-      Hours/Est.
-    </CellMd>
-    <CellMd numeric sm="2">
-      Due Date
-    </CellMd>
-    <Cell numeric xs="5" sm="2">
-      Amount
-    </Cell>
+    </Box>
   </Header>
 );
 
