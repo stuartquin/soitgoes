c9da5f23ca7bb38f547c95f9ad06d3c97391ce7b
Stuart Quin <stuart.quin@gmail.com>
2021-05-22 21:22:58 +0100

    UI testing

---
+++ ui/src/components/TimeSheet/TimeSlip.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+
+import TimeSlip from "./TimeSlip";
+import {
+  TimeSlipContext,
+  TimeSlipEntry,
+} from "components/TimeSheet/TimeSlipContext";
+
+test("loads and displays greeting", async () => {});
---
+++ ui/src/components/TimeSheet/TimeSlipContext.test.ts
@@ -0,0 +1,73 @@
+import { parse } from "date-fns";
+
+import * as models from "api/models";
+import { getTotalsByProject } from "./TimeSlipContext";
+
+describe("getTotalsByProject", () => {
+  test("Week and month totals grouped by project", () => {
+    const projects = [
+      {
+        id: 1,
+      },
+      {
+        id: 2,
+      },
+      {
+        id: 3,
+      },
+    ] as models.Project[];
+    const startDate = parse("2021-03-15", "yyyy-MM-dd", new Date());
+    const timeSlips = [
+      {
+        project: 1,
+        task: 1,
+        user: 1,
+        hours: "8",
+        date: parse("2021-01-01", "yyyy-MM-dd", new Date()),
+      },
+      {
+        project: 1,
+        task: 1,
+        user: 1,
+        hours: "8",
+        date: parse("2021-03-01", "yyyy-MM-dd", new Date()),
+      },
+      {
+        project: 1,
+        task: 1,
+        user: 1,
+        hours: "8",
+        date: parse("2021-03-02", "yyyy-MM-dd", new Date()),
+      },
+      {
+        project: 1,
+        task: 1,
+        user: 1,
+        hours: "8",
+        date: parse("2021-03-16", "yyyy-MM-dd", new Date()),
+      },
+      {
+        project: 2,
+        task: 1,
+        user: 1,
+        hours: "8",
+        date: parse("2021-03-16", "yyyy-MM-dd", new Date()),
+      },
+    ] as models.TimeSlip[];
+
+    expect(getTotalsByProject(startDate, projects, timeSlips)).toEqual({
+      "1": {
+        month: 24,
+        week: 8,
+      },
+      "2": {
+        month: 8,
+        week: 8,
+      },
+      "3": {
+        month: 0,
+        week: 0,
+      },
+    });
+  });
+});
