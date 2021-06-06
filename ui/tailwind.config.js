41ac4d5bbcbeaab0e5fe84492646cebe02470742
Stuart Quin <stuart.quin@gmail.com>
2021-06-06 15:51:50 +0100

    Extends colors

--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "regal-blue": "#243c5a",
+      },
+    },
     minWidth: {
       0: "0",
       "1/4": "25%",
