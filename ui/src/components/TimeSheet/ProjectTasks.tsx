30f619db560607847d092ec6a897484fea5f4db5
Stuart Quin <stuart.quin@gmail.com>
2023-05-19 09:45:10 +0100

    Copy last week time

--- ui/src/components/TimeSheet/ProjectTasks.tsx
+++ ui/src/components/TimeSheet/ProjectTasks.tsx
@@ -1,10 +1,12 @@
-import React, { useMemo } from "react";
-import { format } from "date-fns";
+import React, { useCallback, useContext, useMemo } from "react";
+import { format, sub } from "date-fns";
 
 import * as models from "api/models";
 
 import Task from "components/TimeSheet/Task";
 import { TimeSheetType } from "components/TimeSheet/TimeSlipContext";
+import CopyIcon from "components/CopyIcon";
+import { TimeSlipContext } from "components/TimeSheet/TimeSlipContext";
 
 interface Props {
   tasks: models.Task[];
@@ -23,23 +25,45 @@ function ProjectTasks({
   dateRange,
   timeSheet,
 }: Props) {
+  const { updateHours } = useContext(TimeSlipContext);
   const projectTasks = useMemo(
     () => tasks.filter((t) => t.project === project.id),
-    [tasks, project]
+    [tasks, project, timeSheet]
   );
   const startDate = dateRange[0];
+
+  const handleCopy = useCallback(() => {
+    const projectTime = projectTasks.map((t) => timeSheet[t.id || -1]);
+    dateRange.forEach((date) => {
+      const lastWeek = format(sub(date, { days: 7 }), "yyyy-MM-dd");
+      const current = format(date, "yyyy-MM-dd");
+
+      projectTime.forEach((p) => {
+        const previousTimeslip = p[lastWeek].timeSlip;
+        if (previousTimeslip.hours) {
+          updateHours({ ...p[current] }, previousTimeslip.hours);
+        }
+      });
+    });
+  }, [projectTasks, dateRange, timeSheet, updateHours]);
 
   return (
     <div className="border-1 bg-gray-50 border-radius-sm my-4">
       <div className="bg-gray-100 flex flex-grow flex-wrap py-2 text-gray-700 text-sm md:text-base text-left p-4 justify-between items-center">
         <div className="font-semibold w-full md:w-auto">{project.name}</div>
-        <div className="flex sm:text-right sm:block">
-          <div className="font-semibold text-xs md:text-sm">
-            {weekTotal} hours
+        <div className="flex items-center">
+          <div className="flex sm:text-right sm:block">
+            <div className="font-semibold text-xs md:text-sm">
+              {weekTotal} hours
+            </div>
+            <div className="text-xs text-gray-400 ml-2">
+              {monthTotal} hours in {format(startDate, "MMMM")}
+            </div>
           </div>
-          <div className="text-xs text-gray-400 ml-2">
-            {monthTotal} hours in {format(startDate, "MMMM")}
-          </div>
+          <CopyIcon
+            className="w-6 h-6 ml-2 cursor-pointer"
+            onClick={handleCopy}
+          />
         </div>
       </div>
       {projectTasks.map((task) => (
