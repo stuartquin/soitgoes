111dcff76308542c4691d22995acb65b4c771a14
Stuart Quin <stuart.quin@gmail.com>
2024-12-14 21:42:15 +0000

    Post-upgrade build errors

--- ui/src/components/SlideOver.tsx
+++ ui/src/components/SlideOver.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 interface Props {
-  children: React.ReactChild;
+  children: React.ReactNode;
   isOpen: boolean;
   onClose: () => void;
 }
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,8 +1,5 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const container = document.getElementById("root");
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
